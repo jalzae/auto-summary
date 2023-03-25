7a292ecbe9d7b2a948c571e551717c1ceecb864e
JalzAe <jalzae@gmail.com>
2023-03-25 08:54:41 +0700

    first push

---
+++ src/extension.ts
@@ -0,0 +1,56 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	console.log('Extension activated!');
+
+	const disposable = vscode.commands.registerCommand('document-summary.scanFiles', () => {
+
+		// get all the files in the workspace
+		vscode.workspace.findFiles('**/*').then((uriArray) => {
+
+			// loop through each file
+			uriArray.forEach((fileUri) => {
+
+				// read the file content
+				const fileContent = fs.readFileSync(fileUri.fsPath).toString();
+
+				// search for //()SumStart
+				const startIndex = fileContent.indexOf('//()SumStart');
+				if (startIndex !== -1) {
+
+					// search for //()SumFunc
+					const functionIndex = fileContent.indexOf('//()SumFunc:', startIndex);
+					if (functionIndex !== -1) {
+
+						// search for //()SumEnd
+						const endIndex = fileContent.indexOf('//()SumEnd', functionIndex);
+						if (endIndex !== -1) {
+
+							// extract the function name and code
+							const functionName = fileContent.substring(functionIndex + 12, endIndex).trim();
+							const code = fileContent.substring(startIndex + 13, endIndex).trim();
+
+							// log the function name and code to the console
+							console.log({
+								function: functionName,
+								code: code
+							});
+						}
+					}
+				}
+			});
+		});
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
