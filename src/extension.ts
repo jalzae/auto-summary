9d9abc7023f3995a2d3b1951d7d967d580e3cd7e
jalzae <jalzae@gmail.com>
2023-03-25 18:41:47 +0700

    update view tree with html

--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 		// get all the files in the workspace
 		vscode.workspace.findFiles('**/*').then((uriArray) => {
-
+			let items = [] as MyItem[]
 			// loop through each file
 			uriArray.forEach((fileUri) => {
 
@@ -36,21 +36,57 @@ export function activate(context: vscode.ExtensionContext) {
 							const functionName = fileContent.substring(functionIndex + 12, endIndex).trim();
 							const code = fileContent.substring(startIndex + 13, endIndex).trim();
 
-							// log the function name and code to the console
-							console.log({
+							const item = {
+								url: fileUri.fsPath,
 								function: functionName,
 								code: code
-							});
+							}
+
+							items.push(item)
+
 						}
 					}
 				}
 			});
+
+			showItemList(items)
 		});
 	});
 
 	context.subscriptions.push(disposable);
 }
 
+interface MyItem {
+	url: string;
+	function: string;
+	code: string;
+}
+
+function showItemList(items: MyItem[]) {
+	// Create the webview panel
+	const webviewPanel = vscode.window.createWebviewPanel(
+		'document-summary.itemList',
+		'My Item List',
+		vscode.ViewColumn.Beside,
+		{}
+	);
+
+	// Create an HTML string to display the list
+	let html = '<ul>';
+	for (let item of items) {
+		html += `<li>${item.url}</li>`;
+		html += `<li>${item.function} </li>`;
+		html += `<li>${item.code}</li>`;
+	}
+	html += '</ul>';
+
+	// Set the HTML content of the webview panel
+	webviewPanel.webview.html = html;
+}
+
+
+
+
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
