6804a9ae76ed0138cc2c9ee7588f372440fc3835
JalzAe <jalzae@gmail.com>
2023-07-25 05:37:50 +0700

    remove _ on function api

--- README.md
+++ README.md
@@ -1,25 +1,36 @@
 # document-summary README
 
-This is the README for  extension "document-summary".
+This is the README for extension "document-summary".
 
 ## Features
-1. Scan Summary that result webpage contain function summary code 
-  - //()SumStart
-    Buat diawal kode
-  - //()SumFunc: Nama Fungsi
-  - //()SumRoute: routeFungsi
-  - //()SumMethod: 
-    GET/POST/DELETE/PUT
-  - //()after:
-     ["assign","assignall","dll"]
-  - //()before: 
-     ["fungsi"]
-  - //()body: 
-     {"sample":"sample"}
-  - //()SumEnd 
-    Buat diakhir kode
 
-  ![Preview Image](./Preview.jpg)
+1. Scan Summary that result webpage contain function summary code
 
-2. Generate JSON data for Generate Testing Tools Soon
+- //()SumStart
+  Buat diawal kode
+- //()SumFunc: Nama Fungsi
+- //()SumRoute: routeFungsi
+- //()SumMethod:
+  GET/POST/DELETE/PUT
+- //()after:
+  ["assign","assignall","dll"]
+- //()before:
+  ["fungsi"]
+- //()body:
+  {"sample":"sample"}
+- //()SumEnd
+  Buat diakhir kode
+
+```
+//()SumStart
+//()SumFunc:
+//()SumRoute:
+//()SumMethod:
+//()SumEnd
+```
+
+![Preview Image](./Preview.jpg)
+
+1. Generate JSON data for Generate Testing Tools Soon
    - Command > Generate Test JSON
+
--- src/formatApi.ts
+++ src/formatApi.ts
@@ -1,91 +1,105 @@
-
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
-
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 
 export function formatTs(result: any, extension: string) {
   for (const item of result) {
-    const fileName = item.title.split('\\').pop(); // get the file name with extension
-    const name = fileName.split('.').shift(); // remove the extension and get the name
-    const formatname = name.toLowerCase()
-    let content = `export default {`
+    const fileName = item.title.split("\\").pop(); // get the file name with extension
+    const name = fileName.split(".").shift(); // remove the extension and get the name
+    const formatname = name.toLowerCase();
+    let content = `export default {`;
     for (const i of item.items) {
-      content += `${formatname}${getLastFunction(i.url)}(`
-      //cek :id 
+      content += `${formatname}${getLastFunction(i.url)}(`;
+      //cek :id
       if (checkIdExist(i.url)) {
-        content += `id:any,`
+        content += `id:any,`;
       }
-      //cek post or put 
-      if (i.method == "POST" || i.method == "PUT") content += `data:any`
+      //cek post or put
+      if (i.method == "POST" || i.method == "PUT") content += `data:any`;
       content += `){ 
         return {
-          `
-      content += `method:"${i.method}",`
+          `;
+      content += `method:"${i.method}",`;
       //cek post or put
-      if (i.method == "GET" || i.method == "PUT" || i.method == "POST" || i.method == "DELETE") {
+      if (
+        i.method == "GET" ||
+        i.method == "PUT" ||
+        i.method == "POST" ||
+        i.method == "DELETE"
+      ) {
         if (checkIdExist(i.url)) {
           const replacedStr = i.url.replace("/:id", `"+id`);
-          content += `url:"${replacedStr}`
+          content += `url:"${replacedStr}`;
         } else {
-          content += `url:"${i.url}"`
+          content += `url:"${i.url}"`;
         }
       } else {
-        content += `url:"${i.url}"`
+        content += `url:"${i.url}"`;
       }
-      if (i.method == "POST" || i.method == "PUT") content += `,data`
-      content += `}`
+      if (i.method == "POST" || i.method == "PUT") content += `,data`;
+      content += `}`;
       content += `},
-      `
+      `;
     }
     content += `
-  }`
+  }`;
 
-    const outputString = item.title.replace(/\\[^\\]+$/, '').replace(/\\/g, '/');
-    writeIt(extension, outputString, formatname, content)
+    const outputString = item.title
+      .replace(/\\[^\\]+$/, "")
+      .replace(/\\/g, "/");
+    writeIt(extension, outputString, formatname, content);
   }
 }
 
 export function formatDart(result: any, extension: string = "dart") {
   for (const item of result) {
-    const fileName = item.title.split('\\').pop(); // get the file name with extension
-    const name = fileName.split('.').shift(); // remove the extension and get the name
-    const formatname = name.toLowerCase()
-    const className = formatString(item.title)
-    let content = `class  ${capitalize(className)} {`
+    const fileName = item.title.split("\\").pop(); // get the file name with extension
+    const name = fileName.split(".").shift(); // remove the extension and get the name
+    const formatname = name.toLowerCase();
+    const className = formatString(item.title);
+    let content = `class  ${capitalize(className)} {`;
     for (const i of item.items) {
-      content += `static ${formatname}${getLastFunction(i.url)}(`
-      //cek :id 
+      content += `static ${formatname}${getLastFunction(i.url)}(`;
+      //cek :id
       if (checkIdExist(i.url)) {
-        content += `String id,`
+        content += `String id,`;
       }
-      //cek post or put 
-      if (i.method == "POST" || i.method == "PUT") content += `{Map<String,dynamic> data=const {},Map<String,dynamic> headers=const {}}`
-      content += `){ Map<String,dynamic> request= {`
-      content += `"method":"${i.method}",`
       //cek post or put
-      if (i.method == "GET" || i.method == "PUT" || i.method == "POST" || i.method == "DELETE") {
+      if (i.method == "POST" || i.method == "PUT")
+        content += `{Map<String,dynamic> data=const {},Map<String,dynamic> headers=const {}}`;
+      content += `){ Map<String,dynamic> request= {`;
+      content += `"method":"${i.method}",`;
+      //cek post or put
+      if (
+        i.method == "GET" ||
+        i.method == "PUT" ||
+        i.method == "POST" ||
+        i.method == "DELETE"
+      ) {
         if (checkIdExist(i.url)) {
           const replacedStr = i.url.replace("/:id", `"+id`);
-          content += `"url":"${replacedStr}`
+          content += `"url":"${replacedStr}`;
         } else {
-          content += `"url":"${i.url}"`
+          content += `"url":"${i.url}"`;
         }
       } else {
-        content += `"url":"${i.url}"`
+        content += `"url":"${i.url}"`;
       }
-      if (i.method == "POST" || i.method == "PUT") content += `,"body":data,"header":headers`
-      content += `};`
-      content += `return request;`
+      if (i.method == "POST" || i.method == "PUT")
+        content += `,"body":data,"header":headers`;
+      content += `};`;
+      content += `return request;`;
       content += `}
-      `
-    }  
+      `;
+    }
 
-    content += `}`
-    const outputString = item.title.replace(/\\[^\\]+$/, '').replace(/\\/g, '/');
-    writeIt(extension, outputString, formatname, content)
+    content += `}`;
+    const outputString = item.title
+      .replace(/\\[^\\]+$/, "")
+      .replace(/\\/g, "/");
+    writeIt(extension, outputString, formatname, content);
   }
 }
 
@@ -96,26 +110,34 @@ export function capitalizeFirstLetter(str: string): string {
 export function capitalize(str: string): string {
   const parts = str.split(/(?=[A-Z])/); // split the string by uppercase letters
   parts[0] = capitalizeFirstLetter(parts[0]); // capitalize the first part
-  return parts.join(''); // join the parts back into a string
+  return parts.join(""); // join the parts back into a string
 }
 
 export function formatString(str: string): string {
-  const extension = str.split('.').pop(); // get the file extension
-  str = str.replace(/\\/g, ''); // remove backslashes
-  str = str.replace('.' + extension, ''); // remove file extension
-  str = str.replace('Controllers', ''); // remove 'Controllers'
+  const extension = str.split(".").pop(); // get the file extension
+  str = str.replace(/\\/g, ""); // remove backslashes
+  str = str.replace("." + extension, ""); // remove file extension
+  str = str.replace("Controllers", ""); // remove 'Controllers'
   str = str.charAt(0).toLowerCase() + str.slice(1); // convert first letter to lowercase
   return str;
 }
 
-
-export function writeIt(extension: string, outputString: string, formatname: string, content: string) {
-  const combinedPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath + '/.vscode/' + extension + '/' + outputString;
+export function writeIt(
+  extension: string,
+  outputString: string,
+  formatname: string,
+  content: string
+) {
+  const combinedPath =
+    vscode.workspace.workspaceFolders?.[0].uri.fsPath +
+    "/.vscode/" +
+    extension +
+    "/" +
+    outputString;
   // create the directory if it doesn't exist
   createDirectoryPath(combinedPath);
   fs.writeFileSync(combinedPath + `/${formatname}.${extension}`, content);
 }
-
 
 export function checkIdExist(route: string): boolean {
   const idPattern = /\/:id/;
@@ -124,23 +146,23 @@ export function checkIdExist(route: string): boolean {
 
 export function getLastFunction(route: string) {
   let lastFunction = "";
-  const parts = route.split('/');
+  const parts = route.split("/");
   const lastPart = parts[parts.length - 1];
 
-  if (lastPart.startsWith(':')) {
+  if (lastPart.startsWith(":")) {
     lastFunction = parts[parts.length - 2];
   } else {
     lastFunction = parts[parts.length - 1];
   }
+  lastFunction = lastFunction.replace(/_/g, "");
   return lastFunction;
 }
-
 
 export function createDirectoryPath(dirPath: string) {
   const normalizedPath = path.normalize(dirPath);
   const parts = normalizedPath.split(path.sep);
 
-  let currentPath = '';
+  let currentPath = "";
 
   parts.forEach((part) => {
     currentPath = path.join(currentPath, part);
